e96cd687125875fe12aa1859fcd916bdf9ac35dd
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2022-03-13 17:13:00 +0530

    build react posts submit and add bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import PostCreate from './PostCreate';
 
 
 export default () => {
     return (
-        <div>Blog app </div>
+        <div className="container">
+            <h1>Create Post</h1>
+            <PostCreate />
+        </div>
     );
 };
